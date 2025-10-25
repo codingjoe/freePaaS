f7a48bfabbd7bc9d7bb6887d6d62803dee08013c
Johannes Maron <johannes@maron.family>
2025-10-25 11:21:37 +0200

    Do fix gh argument order

--- bin/install.sh
+++ bin/install.sh
@@ -5,7 +5,7 @@ set -eu
 printf "Enter the name of your project: "
 read -r project_name
 echo "Creating environment form template..."
-gh repo create --template --private codingjoe/freePaaS "${project_name}"
+gh repo create --private --clone --template codingjoe/freePaaS "${project_name}"
 cd "${project_name}" || exit 1
 mv .env.template .env
 
@@ -13,8 +13,8 @@ echo "Setting up your development environment..."
 uv sync --dev
 
 echo "Setting up your production environment..."
-gh secret set POSTGRES_PASSWORD --env production  < python -c "import secrets; print(secrets.token_urlsafe())"
-gh secret set REDIS_PASSWORD --env production  < python -c "import secrets; print(secrets.token_urlsafe())"
+gh secret set POSTGRES_PASSWORD --env production < python -c "import secrets; print(secrets.token_urlsafe())"
+gh secret set REDIS_PASSWORD --env production < python -c "import secrets; print(secrets.token_urlsafe())"
 printf "Enter the hostname for your production environment (e.g., example.com): "
 read -r hostname
 gh variable set HOSTNAME="${hostname}" --env production
