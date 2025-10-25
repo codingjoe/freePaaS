515d8d4d0f549b1916a2ca489ed62b0f86204765
Johannes Maron <johannes@maron.family>
2025-10-25 11:46:01 +0200

    Update install script

--- bin/install.sh
+++ bin/install.sh
@@ -7,17 +7,18 @@ read -r project_name
 echo "Creating environment form template..."
 gh repo create --private --clone --template codingjoe/freePaaS "${project_name}"
 cd "${project_name}" || exit 1
-mv .env.template .env
+mv .env.example .env
 
 echo "Setting up your development environment..."
 uv sync --dev
 
 echo "Setting up your production environment..."
-gh secret set POSTGRES_PASSWORD --env production < python -c "import secrets; print(secrets.token_urlsafe())"
-gh secret set REDIS_PASSWORD --env production < python -c "import secrets; print(secrets.token_urlsafe())"
+gh api -X PUT "/repos/{owner}/{repo}/environments/production" > /dev/null
+python -c "import secrets; print(secrets.token_urlsafe())" | gh secret set POSTGRES_PASSWORD --env production
+python -c "import secrets; print(secrets.token_urlsafe())" | gh secret set REDIS_PASSWORD --env production
 printf "Enter the hostname for your production environment (e.g., example.com): "
 read -r hostname
-gh variable set HOSTNAME="${hostname}" --env production
+echo "$hostname" gh variable set HOSTNAME --env production
 
 echo "Done! Finally setup your Admin user here: https://id.${hostname}/setup"
 open "https://id.${hostname}/setup"
