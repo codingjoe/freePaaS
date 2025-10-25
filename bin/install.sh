a7c413d2186c9698c4e4115c9049e04cf4bcb438
Johannes Maron <johannes@maron.family>
2025-10-25 11:06:54 +0200

    Fix script

--- bin/install.sh
+++ bin/install.sh
@@ -2,9 +2,10 @@
 
 set -eu
 
+read -r "Enter the name of your project: " PROJECT_NAME
 echo "Creating environment form template..."
-gh repo create --template codingjoe/python-container --private
-cd python-container || exit 1
+gh repo create --template --private codingjoe/freePaaS "${PROJECT_NAME}"
+cd "${PROJECT_NAME}" || exit 1
 mv .env.template .env
 
 echo "Setting up your development environment..."
@@ -13,7 +14,7 @@ uv sync --dev
 echo "Setting up your production environment..."
 gh secret set POSTGRES_PASSWORD --env production  < python -c "import secrets; print(secrets.token_urlsafe())"
 gh secret set REDIS_PASSWORD --env production  < python -c "import secrets; print(secrets.token_urlsafe())"
-read -rp "Enter the hostname for your production environment (e.g., example.com): " HOSTNAME
+read -r "Enter the hostname for your production environment (e.g., example.com): " HOSTNAME
 gh variable set HOSTNAME="${HOSTNAME}" --env production
 
 echo "Done! Finally setup your Admin user here: https://id.${HOSTNAME}/setup"
