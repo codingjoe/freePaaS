b91c53e2d54f299336aceaaafbc79cbfd76add91
Johannes Maron <johannes@maron.family>
2025-10-28 15:03:34 +0100

    ssh deployment (#10)
    * Add SSH ed25519 deployment
    
    * Add explicit ssh username

--- bin/install.sh
+++ bin/install.sh
@@ -16,9 +16,25 @@ echo "Setting up your production environment..."
 gh api -X PUT "/repos/{owner}/{repo}/environments/production" > /dev/null
 python -c "import secrets; print(secrets.token_urlsafe())" | gh secret set POSTGRES_PASSWORD --env production
 python -c "import secrets; print(secrets.token_urlsafe())" | gh secret set REDIS_PASSWORD --env production
+
 printf "Enter the hostname for your production environment (e.g., example.com): "
 read -r hostname
+
+printf "Enter the SSH username for your production server: "
+read -r ssh_username
+
+# Test SSH connection
+ssh -T "${ssh_username}@${hostname}" "echo 'SSH connection to ${hostname} successful.'"
+
 echo "$hostname" gh variable set HOSTNAME --env production
+echo "$ssh_username" gh variable set SSH_USERNAME --env production
+
+# Create SSH_PRIVATE_KEY
+KEY_PATH="$(mktemp -d)"
+ssh-keygen -t ed25519 -C "freePaaS deployment key" -f "${KEY_PATH}/deploy_key" -N ""
+gh secret set SSH_PRIVATE_KEY --env production < "${KEY_PATH}/deploy_key"
+gh secret set SSH_PUBLIC_KEY --env production < "${KEY_PATH}/deploy_key.pub"
+ssh-copy-id -i "${KEY_PATH}/deploy_key.pub" "${ssh_username}@${hostname}"
 
 # Create SSH_PRIVATE_KEY
 KEY_PATH="$(mktemp -d)"
