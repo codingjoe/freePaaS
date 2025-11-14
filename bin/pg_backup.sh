b07723ecf4b7db1d1a2000e48aad6264266ed705
Johannes Maron <johannes@maron.family>
2025-11-14 15:58:40 +0100

    Resolve #4 -- Add backup service

--- bin/pg_backup.sh
+++ bin/pg_backup.sh
@@ -4,6 +4,7 @@ set -eu
 # A script to backup a PostgreSQL database using pg_dump.
 
 printf "Backing up PostgreSQL database..."
+mkdir -p ./backups
 FILENAME="./backups/$(date -u +%Y-%m-%dT%H:%M:%SZ).dump"
 until docker compose exec postgres /usr/bin/pg_dump -Rc -U postgres postgres > "$FILENAME"
 do
